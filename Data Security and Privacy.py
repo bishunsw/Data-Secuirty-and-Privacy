97dfd024d61aacced38d4cd1f856bf14440bd0e7
bishunsw <85056033+bishunsw@users.noreply.github.com>
2021-06-03 15:26:40 +1000

    Update to .py (Fix Option 3) V2.12

--- Data Security and Privacy.py
+++ Data Security and Privacy.py
@@ -248,6 +248,7 @@ while True:                                                                     
         print("-" * 80)                                                         #This creates 80 dash symbols to tweak the result view in the terminal
         print("-" * 80)                                                         #This creates 80 dash symbols to tweak the result view in the terminal
         options()                                                               #This calls the option function which displays all the options in this tool
+        option = input ("What option would you like? (Please type the option number - Like '1') - ")                #This prompts the user to select an option via their input in the terminal
     elif option == "4":
         clean()                                                                 #This calls the clean function that "cleans" the terminal of all previous messages so that the terminal is empty
         portscanner3()                                                          #This calls the portscanner 3 function that starts the option for scanning a another host and selecting the minimum and max port number
